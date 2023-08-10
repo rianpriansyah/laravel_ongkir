7cae02fca3746ffea5aac881d8ea5ea1128034d3
Rian Priansyah <rian.priansyah04@gmail.com>
2023-08-10 17:50:59 +0700

    membuat fungsi dependent dropdown

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -63,7 +63,10 @@
                         <div class="form-group">
                             <label for="">Provinsi</label>
                             <select name="origin_province" id="" class="form-control">
-                                <option value="#">-</option>
+                                <option value="#">Pilih Provinsi</option>
+                                @foreach ($province as $key => $value)
+                                    <option value="{{ $key }}">{{ $value }}</option>
+                                @endforeach
                             </select>
                         </div>
                         <div class="form-group">
